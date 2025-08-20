641f3a55fbb6a3333802a361f8338ec4a8adf9f9
carlosjuare <119368143+carlosjuare@users.noreply.github.com>
2025-08-19 21:19:13 -0300

    Delete login directory

--- login/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset(); // Limpia todas las variables de sesión
-session_destroy(); // Destruye la sesión actual
-
-// Redirige al inicio
-header("Location: ../vistageneral/presentacion.php");
-exit();
